dbbac3e616a5c9f06cab33a60ceba5e50ebc4bed
Yonathan Castillo <leothan522@gmail.com>
2025-10-05 18:29:24 -0400

    Composer update and Npm update

--- resources/views/web/layouts/topbar.blade.php
+++ resources/views/web/layouts/topbar.blade.php
@@ -1,7 +1,7 @@
 <div class="topbar d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-speedometer2 d-flex align-items-center"><a href="{{ route('filament.dashboard.pages.dashboard') }}">{{ __('Dashboard') }}</a></i>
+            <i class="bi bi-speedometer2 d-flex align-items-center"><a href="{{ route('filament.dashboard.pages.dashboard') }}">Panel Administrativo</a></i>
             <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">

665ccb4a5a1604522a9471e20c354bc9cd2993a9
Yonathan Castillo <leothan522@gmail.com>
2025-10-11 12:18:20 -0400

    Modulo Pacientes Actualizado

--- app/Filament/Resources/Pacientes/PacienteResource.php
+++ app/Filament/Resources/Pacientes/PacienteResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\Pacientes\Pages\ManagePacientes;
 use App\Models\Paciente;
 use BackedEnum;
 use Carbon\Carbon;
+use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
@@ -19,6 +20,7 @@ use Filament\Actions\ViewAction;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Resource;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Get;
@@ -30,6 +32,7 @@ use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class PacienteResource extends Resource
 {
@@ -82,7 +85,8 @@ class PacienteResource extends Resource
                     ])
                     ->compact()
                     ->columns(3)
-                    ->columnSpanFull(),
+                    ->columnSpanFull()
+                    ->collapsible(),
                 Section::make('Datos Médicos')
                     ->schema([
                         DatePicker::make('fur')
@@ -104,7 +108,8 @@ class PacienteResource extends Resource
                     ])
                     ->compact()
                     ->columns(3)
-                    ->columnSpanFull(),
+                    ->columnSpanFull()
+                    ->collapsible(),
             ]);
     }
 
@@ -116,7 +121,8 @@ class PacienteResource extends Resource
                 TextColumn::make('paciente')
                     ->numeric()
                     ->default(fn(Paciente $record): string => $record->cedula)
-                    ->description(fn(Paciente $record): string => $record->nombre)
+                    ->description(fn(Paciente $record): string => Str::upper($record->nombre))
+                    ->wrap()
                     ->hiddenFrom('md'),
                 TextColumn::make('cedula')
                     ->label('Cédula')
@@ -125,6 +131,7 @@ class PacienteResource extends Resource
                     ->visibleFrom('md'),
                 TextColumn::make('nombre')
                     ->label('Nombre y Apellido')
+                    ->formatStateUsing(fn(string $state): string => Str::upper($state))
                     ->searchable()
                     ->visibleFrom('md'),
                 TextColumn::make('fecha_nacimiento')
@@ -157,9 +164,20 @@ class PacienteResource extends Resource
             ])
             ->recordActions([
                 ActionGroup::make([
-                    ViewAction::make(),
-                    EditAction::make(),
-                    DeleteAction::make(),
+                    ViewAction::make()
+                        ->modalHeading(fn(Paciente $record): string => 'Vista de ' . formatoMillares($record->cedula, 0))
+                        ->modalDescription(fn(Paciente $record): string => Str::upper($record->nombre))
+                        ->extraModalFooterActions([
+                            EditAction::make()
+                                ->modalHeading(fn(Paciente $record): string => 'Editar ' . formatoMillares($record->cedula, 0))
+                                ->modalDescription(fn(Paciente $record): string => Str::upper($record->nombre))
+                        ]),
+                    EditAction::make()
+                        ->modalHeading(fn(Paciente $record): string => 'Editar ' . formatoMillares($record->cedula, 0))
+                        ->modalDescription(fn(Paciente $record): string => Str::upper($record->nombre))
+                    ,
+                    DeleteAction::make()
+                    ->modalHeading(fn(Paciente $record): string => 'Borrar '.formatoMillares($record->cedula, 0)),
                     ForceDeleteAction::make(),
                     RestoreAction::make(),
                 ])
@@ -170,6 +188,9 @@ class PacienteResource extends Resource
                     ForceDeleteBulkAction::make(),
                     RestoreBulkAction::make(),
                 ]),
+                Action::make('actualizar')
+                    ->icon(Heroicon::ArrowPath)
+                    ->iconButton(),
             ]);
     }
 
@@ -187,4 +208,86 @@ class PacienteResource extends Resource
                 SoftDeletingScope::class,
             ]);
     }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Datos Básicos')
+                    ->schema([
+                        TextEntry::make('cedula')
+                            ->label('Cédula')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->cedula)
+                            ->numeric()
+                            ->color('primary')
+                            ->copyable(),
+                        TextEntry::make('nombre')
+                            ->label('Nombre y Apellido')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->nombre)
+                            ->formatStateUsing(fn(string $state): string => Str::upper($state))
+                            ->color('primary')
+                            ->columnSpan(2)
+                            ->copyable(),
+                        TextEntry::make('fecha_nacimiento')
+                            ->label('Fecha Nac.')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->fecha_nacimiento)
+                            ->date('d/m/Y')
+                            ->color('primary')
+                            ->copyable(),
+                        TextEntry::make('edad')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->edad)
+                            ->numeric()
+                            ->color('primary')
+                            ->copyable(),
+                        TextEntry::make('telefono')
+                            ->label('Teléfono')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->telefono)
+                            ->color('primary')
+                            ->copyable(),
+                        TextEntry::make('direccion')
+                            ->label('Dirección')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->direccion)
+                            ->formatStateUsing(fn(string $state): string => Str::upper($state))
+                            ->color('primary')
+                            ->columnSpanFull()
+                            ->copyable()
+                    ])
+                    ->compact()
+                    ->columns(3)
+                    ->collapsible(),
+                Section::make('Datos Médicos')
+                    ->schema([
+                        TextEntry::make('fur')
+                            ->label('FUR')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->fur)
+                            ->date()
+                            ->color('primary'),
+                        TextEntry::make('fpp')
+                            ->label('FPP')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->fpp)
+                            ->date()
+                            ->color('primary'),
+                        TextEntry::make('gestas')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->gestas)
+                            ->numeric()
+                            ->color('primary'),
+                        TextEntry::make('partos')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->partos)
+                            ->numeric()
+                            ->color('primary'),
+                        TextEntry::make('cesareas')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->cesareas)
+                            ->numeric()
+                            ->color('primary'),
+                        TextEntry::make('abortos')
+                            ->getStateUsing(fn(Paciente $record): ?string => Paciente::find($record->id)?->abortos)
+                            ->numeric()
+                            ->color('primary'),
+                    ])
+                    ->compact()
+                    ->columns(3)
+                    ->collapsible()
+            ]);
+    }
+
 }
